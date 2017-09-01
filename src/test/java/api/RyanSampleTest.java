bc3c290a639bff6f6883e0dbc8cd4bea46040e94
Ryan Ramphal <rramphal94@gmail.com>
2017-09-01 00:43:50 -0400

    Temp failing test to verify that Circle CI will detect the failed test and update status badge accordingly.

---
+++ src/test/java/api/RyanSampleTest.java
@@ -0,0 +1,12 @@
+package api;
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+
+public class RyanSampleTest {
+
+    @Test
+    public void alwaysFails(){
+        assertFalse(true);
+    }
+}
